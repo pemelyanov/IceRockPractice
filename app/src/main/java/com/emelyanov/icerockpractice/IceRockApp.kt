215bf23a329a13926fdfac8e7150dbd7b60408d6
Pavel Emelyanov <p.emelyanov@mail.ru>
2022-05-11 22:53:59 +0700

    Базовая настройка проекта

---
+++ app/src/main/java/com/emelyanov/icerockpractice/IceRockApp.kt
@@ -0,0 +1,7 @@
+package com.emelyanov.icerockpractice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IceRockApp : Application()
---
+++ app/src/main/java/com/emelyanov/icerockpractice/modules/core/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.emelyanov.icerockpractice.modules.core.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emelyanov.icerockpractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
